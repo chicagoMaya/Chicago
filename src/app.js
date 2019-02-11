a8912c7e32e7e2bc6c4368a673867ba8147e76fb
chicagoMaya <47531366+chicagoMaya@users.noreply.github.com>
2019-02-11 16:30:34 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,26 @@
+document.getElementById('get-crazy').addEventListener('click', getCrazy);
+document.getElementById('get-assets').addEventListener('click', getAssets);
+
+function getCrazy() {
+  document.getElementById('party').style.background = `url('src/img/8.jpg') no-repeat top center`;
+  document.getElementById('party').style.backgroundSize = 'cover';
+  document.getElementById('party-dialogue').textContent = 'Hey there beautiful, It\'s your birthday, wanna drink Bacardi?';
+  document.getElementById('get-crazy').setAttribute('id', 'drink-bacardi');
+  document.getElementById('drink-bacardi').textContent = 'Drink Bacardi';
+  document.getElementById('drink-bacardi').addEventListener('click', drinkBacardi);
+}
+
+function drinkBacardi() {
+  document.getElementById('party').style.background = `url('src/img/10.jpg') no-repeat bottom left`;
+  document.getElementById('party').style.backgroundSize = 'cover';
+  document.getElementById('drink-bacardi').style.display = 'none';
+  document.getElementById('party-dialogue').textContent = 'Uhh, My head... what happened? I need to go to work soon...';
+}
+
+function getAssets() {
+  document.getElementById('office').style.background = `url('src/img/12.jpg') no-repeat top center`;
+  document.getElementById('office').style.backgroundSize = 'cover';
+  document.getElementById('office-dialogue').textContent = 'I\'m on it, here you go';
+  document.getElementById('get-assets').style.display = 'none';
+  document.getElementById('thank-you').style.display = 'inline-block';
+}
