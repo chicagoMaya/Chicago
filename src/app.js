9248897b2864cface92a6b2b2136986c72c59abe
chicagoMaya <47531366+chicagoMaya@users.noreply.github.com>
2019-02-12 11:44:18 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ function drinkBacardi() {
   document.getElementById('party').style.backgroundSize = 'cover';
   document.getElementById('drink-bacardi').style.display = 'none';
   document.getElementById('disclaimer').style.display = 'none';
-  document.getElementById('party-dialogue').textContent = 'Uhh, My head... what happened? I need to go to work soon...';
+  document.getElementById('party-dialogue').textContent = 'Happy birthday, Maja!';
 }
 
 function getAssets() {
